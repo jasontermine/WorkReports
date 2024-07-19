92eaf4e2f000d09e9a51ed0063e50b5854b8595e
jasontermine <jason.termine@wiss-edu.ch>
2024-07-19 10:13:24 +0200

    UPDATED: updated Cors origin url for frontend and added automatic database migration

--- backend/WebApi/Program.cs
+++ backend/WebApi/Program.cs
@@ -14,13 +14,27 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowSpecificOrigin",
         policy =>
         {
-            policy.WithOrigins("http://localhost:5173")
+            policy.WithOrigins("http://localhost:3000")
                   .AllowAnyHeader()
                   .AllowAnyMethod();
         });
 });
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    try
+    {
+        dbContext.Database.Migrate();
+    }
+    catch (Exception ex)
+    {
+        var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred while applying migrations.");
+    }
+}
 
 if (app.Environment.IsDevelopment())
 {
