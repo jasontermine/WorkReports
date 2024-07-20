41aea3e6150fe5807a416cc9202b0e29945d47fc
jasontermine <jason.termine@wiss-edu.ch>
2024-07-20 12:29:47 +0200

    UPDATED: added vue-router and vuetify to app init

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,10 +1,24 @@
 import '@/assets/main.css'
 import { createApp } from 'vue'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 import App from '@/App.vue'
 import pinia from '@/domain/pinia'
+import router from "@/router/index";
 
 const app = createApp(App)
 
+const vuetify = createVuetify({
+    components,
+    directives,
+    theme: {
+      defaultTheme: 'dark',
+    }
+  })
+
 app.use(pinia)
+app.use(vuetify)
+app.use(router);
 
 app.mount('#app')
