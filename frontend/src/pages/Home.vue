31ab1d672b24202ca14711d35b37cb8349eb5571
jasontermine <jason.termine@wiss-edu.ch>
2024-07-21 12:18:47 +0200

    UPDATED: render AssignmentForm

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -1,9 +1,11 @@
 <template>
+  <h1>Create an Assignment:</h1>
+
+  <AssignmentForm />
 </template>
 
 <script setup lang="ts">
+import AssignmentForm from '@/components/AssignmentForm.vue'
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
